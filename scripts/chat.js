01b210ebd224a12d08d4122b035cce79ff166b8c
Billy O'Connor <oconnor.iam@gmail.com>
2020-03-12 12:25:29 +0000

    App completed

--- scripts/chat.js
+++ scripts/chat.js
@@ -3,6 +3,7 @@ class Chatroom {
         this.room = room;
         this.username = username;
         this.chats = db.collection('chats');
+        this.unsub;
     }
     async addChat(message) {
         // format a chat object
@@ -17,10 +18,31 @@ class Chatroom {
         const response = await this.chats.add(chat);
         return response;
     }
+    getChats(callback) {
+        this.unsub = this.chats
+        .where('room', '==', this.room)
+        .orderBy('created_at')
+        .onSnapshot(snapshot => {
+            snapshot.docChanges().forEach(change => {
+                if(change.type === 'added') {
+                    // update the ui
+                    callback(change.doc.data());
+                }
+            })
+        });
+    }
+    updateName(username) {
+        this.username = username;
+        localStorage.setItem('username', username);
+    }
+    updateRoom(room) {
+        this.room = room;
+        console.log('room updated');
+        if (this.unsub) {
+            this.unsub();
+        }
+    }
 }
 
-const chatroom = new Chatroom('gaming', 'billy');
 
-chatroom.addChat('hello everyone')
-.then(() => console.log('chat added'))
-.catch(err => console.log(err));
+
